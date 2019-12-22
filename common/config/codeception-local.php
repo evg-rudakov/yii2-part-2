5915786a1818c32d8193718d224bddab969cc9d0
efrony <efron.vit@gmail.com>
2019-12-22 16:07:00 +0300

    lesson 1 start

---
+++ common/config/codeception-local.php
@@ -0,0 +1,16 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'HP_u1Jv_aYY-7kUXKCxn10pxAaVcziVn',
+            ],
+        ],
+    ]
+);
